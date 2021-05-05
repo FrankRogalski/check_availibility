7b34468718229a26aa84108407327b30610bb719
frankrogalski <frank.rogalski@hansa-flex.com>
2021-05-05 08:40:17 +0200

    reading request form data

--- main.py
+++ main.py
@@ -156,7 +156,7 @@ def hello_world():
 
 @app.route("/sendmail", methods=["POST"])
 def sendmail():
-    app.logger.warning("%s swag yolo", request.args.get("test", type=str))
+    app.logger.warning("%s swag yolo", str(request.form.to_dict()))
     return Response(status=200)
 
 if __name__ == '__main__':
