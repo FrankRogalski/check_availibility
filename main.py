bfeef058604b22aaa749fced73328f71d1a801d2
frankrogalski <frank.rogalski@hansa-flex.com>
2021-05-12 10:50:50 +0200

    shortened sql connection time

--- main.py
+++ main.py
@@ -20,38 +20,38 @@ def form(num, _):
     return {1: "Online", 0.5: "Lokale Probleme", 0: "Offline"}[num]
 
 def read_data(start, end):
+    start = datetime.strptime(start, usr_time).strftime(db_time)
+    end = datetime.strptime(end, usr_time).strftime(db_time)
     with sqlite3.connect(db_name) as con:
-        start = datetime.strptime(start, usr_time).strftime(db_time)
-        end = datetime.strptime(end, usr_time).strftime(db_time)
         df = pd.read_sql(f"SELECT * FROM availability WHERE date > '{start}' and date < '{end}'", con, parse_dates=["date"], index_col="date")
-        data = df.apply(type_to_number, axis=1)
-        ax = data.plot(
-            figsize=(20, 10),
-            grid=True,
-            legend=False,
-            yticks=(0, 0.5, 1),
-            title="Graph der System Verfügbarkeit",
-            ylabel="System Verfügbarkeit",
-            xlabel="Zeitpunkt",
-            ylim=(-0.1, 1.1),
-            rot=0,
-        )
-        ax.get_yaxis().set_major_formatter(FuncFormatter(form))
-        ax.get_xaxis().set_major_formatter(DateFormatter(display_time))
-        byte = BytesIO()
-        f = ax.get_figure()
-        f.savefig(byte, bbox_inches='tight', format="jpg")
-        byte.seek(0)
-        img = base64.b64encode(byte.read()).decode()
-        f.clear()
-        plt.close(f)
-        max_index = data.index.max()
-        newest_data = data[max_index]
-        try:
-            newest_data = newest_data.iloc[-1]
-        except AttributeError:
-            pass
-        return options[newest_data], img, [i.strftime(display_time) for i in data[data == 0].index]
+    data = df.apply(type_to_number, axis=1)
+    ax = data.plot(
+        figsize=(20, 10),
+        grid=True,
+        legend=False,
+        yticks=(0, 0.5, 1),
+        title="Graph der System Verfügbarkeit",
+        ylabel="System Verfügbarkeit",
+        xlabel="Zeitpunkt",
+        ylim=(-0.1, 1.1),
+        rot=0,
+    )
+    ax.get_yaxis().set_major_formatter(FuncFormatter(form))
+    ax.get_xaxis().set_major_formatter(DateFormatter(display_time))
+    byte = BytesIO()
+    f = ax.get_figure()
+    f.savefig(byte, bbox_inches='tight', format="jpg")
+    byte.seek(0)
+    img = base64.b64encode(byte.read()).decode()
+    f.clear()
+    plt.close(f)
+    max_index = data.index.max()
+    newest_data = data[max_index]
+    try:
+        newest_data = newest_data.iloc[-1]
+    except AttributeError:
+        pass
+    return options[newest_data], img, [i.strftime(display_time) for i in data[data == 0].index]
 
 if __name__ == '__main__':
     matplotlib.use('Agg')
